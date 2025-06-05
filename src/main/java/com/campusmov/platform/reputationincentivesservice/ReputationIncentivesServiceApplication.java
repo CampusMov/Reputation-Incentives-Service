2e60b001c93209502cb7d5ffbbbbf726c8369274
Eric <erich56000@gmail.com>
2025-06-05 16:14:44 -0500

    chore: init commit

---
+++ src/main/java/com/campusmov/platform/reputationincentivesservice/ReputationIncentivesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.campusmov.platform.reputationincentivesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReputationIncentivesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReputationIncentivesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campusmov/platform/reputationincentivesservice/ReputationIncentivesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusmov.platform.reputationincentivesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReputationIncentivesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
