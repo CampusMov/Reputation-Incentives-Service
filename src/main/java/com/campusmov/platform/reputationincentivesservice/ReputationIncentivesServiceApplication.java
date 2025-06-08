73d8636a807403b1d8dec78fad14dd4d83fffd5b
Eric <erich56000@gmail.com>
2025-06-08 11:58:49 -0500

    chore: implement application properties and enable jpa auditing

--- src/main/java/com/campusmov/platform/reputationincentivesservice/ReputationIncentivesServiceApplication.java
+++ src/main/java/com/campusmov/platform/reputationincentivesservice/ReputationIncentivesServiceApplication.java
@@ -2,7 +2,9 @@ package com.campusmov.platform.reputationincentivesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ReputationIncentivesServiceApplication {
 
