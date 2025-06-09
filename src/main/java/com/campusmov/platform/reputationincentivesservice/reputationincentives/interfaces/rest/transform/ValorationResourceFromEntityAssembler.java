7bfe834b0318cd5f9276e6e1931afa8e2c1b964f
Eric <erich56000@gmail.com>
2025-06-08 19:12:22 -0500

    feat(valoration): add valoration resource and transform interface

---
+++ src/main/java/com/campusmov/platform/reputationincentivesservice/reputationincentives/interfaces/rest/transform/ValorationResourceFromEntityAssembler.java
@@ -0,0 +1,15 @@
+package com.campusmov.platform.reputationincentivesservice.reputationincentives.interfaces.rest.transform;
+
+import com.campusmov.platform.reputationincentivesservice.reputationincentives.domain.model.aggregates.Valoration;
+import com.campusmov.platform.reputationincentivesservice.reputationincentives.interfaces.rest.resources.ValorationResource;
+
+public class ValorationResourceFromEntityAssembler {
+    public static ValorationResource toResourceFromEntity(Valoration valoration) {
+        return new ValorationResource(
+                valoration.getUserId(),
+                valoration.getSenderId(),
+                valoration.getReputationScore(),
+                valoration.getMessage()
+        );
+    }
+}
