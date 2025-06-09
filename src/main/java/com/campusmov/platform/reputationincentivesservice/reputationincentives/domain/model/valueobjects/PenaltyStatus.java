b3626606e3809753eebfb6bbfdd56f903b56c8be
Eric <erich56000@gmail.com>
2025-06-08 22:54:00 -0500

    feat(infraction): add Penalty entity and status and type enums

---
+++ src/main/java/com/campusmov/platform/reputationincentivesservice/reputationincentives/domain/model/valueobjects/PenaltyStatus.java
@@ -0,0 +1,8 @@
+package com.campusmov.platform.reputationincentivesservice.reputationincentives.domain.model.valueobjects;
+
+public enum PenaltyStatus {
+    ACTIVE,
+    DISMISSED,
+    EXPIRED,
+    COMPLETED
+}
---
+++ src/main/java/com/campusmov/platform/reputationincentivesservice/reputationincentives/domain/model/valueobjects/PenaltyType.java
@@ -0,0 +1,8 @@
+package com.campusmov.platform.reputationincentivesservice.reputationincentives.domain.model.valueobjects;
+
+public enum PenaltyType {
+    WARNING,
+    FINE,
+    SUSPENSION,
+    PERMANENT_BAN,
+}
